95f64335d75a8a93733b8432e3fe147389124731
Igor Zheleznov <negozhe@gmail.com>
2013-08-29 14:54:52 +0400

    base paging interface

--- paging/app.js
+++ paging/app.js
@@ -1,59 +1,96 @@
+/// <reference path="jquery.d.ts" />
+/// <reference path="backbone.d.ts" />
+/// <reference path="underscore.d.ts" />
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-/// <reference path="jquery.d.ts" />
-/// <reference path="backbone.d.ts" />
-/// <reference path="underscore.d.ts" />
 var Paging;
 (function (Paging) {
     var PagingModel = (function (_super) {
         __extends(PagingModel, _super);
         function PagingModel() {
             _super.apply(this, arguments);
-            this.defaults = {
-                isAjax: false
-            };
         }
+        PagingModel.prototype.defaults = function () {
+            return {
+                pageCount: 14,
+                pageShown: 5,
+                activePage: 1
+            };
+        };
+
+        PagingModel.prototype.validate = function (attributes, options) {
+            if (attributes.activePage > attributes.pageCount || attributes.activePage < 1)
+                return "Invalid page";
+        };
+
+        PagingModel.prototype.isStart = function () {
+            return this.get('activePage') === 1;
+        };
+
+        PagingModel.prototype.isEnd = function () {
+            return this.get('activePage') === this.get('pageCount');
+        };
+
+        PagingModel.prototype.startPage = function () {
+            return 1 + ~~((this.get('activePage') - 1) / this.get('pageShown')) * this.get('pageShown');
+        };
+
+        PagingModel.prototype.endPage = function () {
+            var p = this.startPage() + this.get('pageShown') - 1, pCount = this.get('pageCount');
+            return p > pCount ? pCount : p;
+        };
+
+        PagingModel.prototype.toJSON = function () {
+            var res = _(this.attributes).clone();
+
+            res.isStart = this.isStart();
+            res.isEnd = this.isEnd();
+            res.startPage = this.startPage();
+            res.endPage = this.endPage();
+
+            return res;
+        };
         return PagingModel;
     })(Backbone.Model);
+    Paging.PagingModel = PagingModel;
 
     var PagingView = (function (_super) {
         __extends(PagingView, _super);
         function PagingView() {
             _super.apply(this, arguments);
-            this.template = _.template($('#paging').html());
         }
+        PagingView.prototype.events = function () {
+            return {
+                'click a': 'onClick'
+            };
+        };
+
+        PagingView.prototype.initialize = function () {
+            this.listenTo(this.model, 'change:activePage', this.render);
+        };
+
+        PagingView.prototype.render = function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        };
+
+        PagingView.prototype.onClick = function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.model.set({ activePage: $(e.currentTarget).data('page') }, { validate: true });
+        };
         return PagingView;
     })(Backbone.View);
+    Paging.PagingView = PagingView;
+
+    PagingView.prototype.template = _.template($('#paging-template').html());
 })(Paging || (Paging = {}));
 
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
-    };
-
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-})();
-
-window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+var m = new Paging.PagingModel();
+var v = new Paging.PagingView({ el: "#content", model: m });
+v.render();
 //# sourceMappingURL=app.js.map
--- paging/app.ts
+++ paging/app.ts
@@ -1,45 +1,81 @@
 /// <reference path="jquery.d.ts" />
 /// <reference path="backbone.d.ts" />
 /// <reference path="underscore.d.ts" />
-module Paging {
-    class PagingModel extends Backbone.Model {
 
-        defaults {
-            isAjax: false
+module Paging {
+    export class PagingModel extends Backbone.Model {
+        defaults() {
+            return {
+                pageCount: 14,
+                pageShown: 5,
+                activePage: 1
+            };
+        }
+
+        validate(attributes: any, options?: any ) {
+            if (attributes.activePage > attributes.pageCount || attributes.activePage < 1)
+                return "Invalid page";
+        }
+
+
+        isStart() {
+            return this.get('activePage') === 1;
+        }
+
+        isEnd() {
+            return this.get('activePage') === this.get('pageCount');
+        }
+
+        startPage() {
+            return 1 + ~~((this.get('activePage') - 1) / this.get('pageShown')) * this.get('pageShown');
+        }
+
+        endPage() {
+            var p: number = this.startPage() + this.get('pageShown') - 1,
+                pCount: number = this.get('pageCount'); 
+            return p > pCount ? pCount : p;
+        }
+
+        toJSON() {
+            var res = _(this.attributes).clone();
+
+            res.isStart = this.isStart();
+            res.isEnd = this.isEnd();
+            res.startPage = this.startPage();
+            res.endPage = this.endPage();
+
+            return res;
         }
     }
 
-    class PagingView extends Backbone.View {
-        template = _.template($('#paging').html())
+    export class PagingView extends Backbone.View {
+        template: (data: any) => string;
+
+        events() {
+            return {
+                'click a': 'onClick'
+            };
+        }
+
+        initialize() {
+            this.listenTo(this.model, 'change:activePage', this.render);
+        }
+
+        render() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+
+        onClick(e: Event) {
+            e.stopPropagation();
+            e.preventDefault();
+            this.model.set({ activePage: $(e.currentTarget).data('page') }, {validate: true});
+        }
     }
+
+    PagingView.prototype.template = _.template($('#paging-template').html());
 }
 
-
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+var m = new Paging.PagingModel();
+var v = new Paging.PagingView({ el: "#content", model: m });
+v.render();
